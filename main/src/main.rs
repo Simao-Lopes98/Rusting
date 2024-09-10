a3e7f718c4886a66d9d8524f26cba8839729c2e9
Simão Lopes <simaolopes98@gmail.com>
2024-09-10 20:55:23 +0100

    Varaible Declaration and operations

--- main/src/main.rs
+++ main/src/main.rs
@@ -6,5 +6,10 @@
 *
 */
 fn main() {
-    println!("Hello, world!");
+    let x: f32 = 10.0; // "mut" is used to let the compiler know that x can be reassigned. 
+                          // ": u16" lets the compiler what type x is
+    let y: u8 = 3;
+    println!("x is {}, y is {}",x, y);
+    let z: f32 = x / y as f32;
+    println!("z is {}",z);
 }
