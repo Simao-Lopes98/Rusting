85b78df84b376253c88e345143c0d6e4f5fdaf31
Simão Lopes <simaolopes98@gmail.com>
2024-11-17 15:09:47 +0000

    Arrasy lesson

--- main/src/main.rs
+++ main/src/main.rs
@@ -6,33 +6,16 @@
 *
 */
 fn main() {
-    let x: f32 = 10.0; // "mut" is used to let the compiler know that x can be reassigned. 
-                        // ": u16" lets the compiler what type x is
-    let y: u8 = 3;
-    println!("x is {}, y is {}",x, y);
-    let z: f32 = x / y as f32; // use "as" to type cast
-    println!("z is {z}");
 
-    let mut dio_register:u8 = 0b000_1111;
-    println!("Value DIO {dio_register:0b}");
-    dio_register = !dio_register;
-    println!("Not DIO {dio_register:0b}");
-    dio_register |= 0b0100_1001;
-    println!("Bitwise-OR DIO {dio_register:0b}");
-    println!("Bitwise-AND DIO {:0b}", dio_register & !(1 << 5));
-    // panic!(); // used to panic. program exits
+    let mut letters: [char; 3] = ['a','b','c'];
+    letters[0] = 'x';
+    let f_letters: char = letters[0];
 
-    // Comparisons and boolean data type
-    let a:  bool = true;
-    let b:  bool = false;
-    println!("a is {}. b is {}", a, b);
-    println!("a LEES THAN b is {}", a < b);
-    println!("a GREATER THAN b is {}", a > b);
+    println!("{}", f_letters);
 
-    // Char data type. Char uses 4bytes
-    let letter:char = 'a';
-    let number:char = '1';
-    let finger:char = '\u{261D}';
+    let numbers: [i32; 5]; //Init empty array of i32 with length 5
+    numbers = [0;5]; //Fill the array wth 0
+    let index: usize = numbers.len();
+    println!("{}",numbers[index]);
 
-    println!("{letter}\n{number}\n{finger}");
-}
+    }
