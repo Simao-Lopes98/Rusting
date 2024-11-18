6b2197afb824b010c6b832fc01a5d762c111812e
Simão Lopes <simaolopes98@gmail.com>
2024-11-18 20:40:12 +0000

    Function challenge

--- main/src/main.rs
+++ main/src/main.rs
@@ -9,29 +9,12 @@ use std::result;
 *
 */
 fn main() {
-    say_hello();
-    say_hello();
-    let y: (i32, i32) = square(13);
-    println!("Result {:?}", y);
+    let celsius_temp: f64 = 23.0;
+    let fhar_temp: f64 = celsius_to_fah(celsius_temp);
+
+    println!("Converted temp: {}", fhar_temp);
     }
 
-fn say_hello() { // Function declaration. Rust doesnt care if there is no prototype.
-    println!("Hello");
-    say_a_number(32);
+fn celsius_to_fah(cesl:f64) -> f64{
+    return ((1.8 * cesl) + 32 as f64);
 }
-
-fn say_a_number(number:i32){
-    println!("number is {number}");
-}
-
-fn say_sum(a:u8, b:u8){
-    let sum: u8 = a + b;
-    println!("Sum {sum}");    
-}
-
-fn square(x:i32) -> (i32, i32) { // Return example
-    println!("Squaring");
-    //x * x // Returns if it is the last line.
-    return (x, x * x);
-}
-
