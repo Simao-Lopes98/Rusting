685ae287626dced2aa435abdb36a50aaf27ee14a
Simão Lopes <simaolopes98@gmail.com>
2024-11-24 17:32:13 +0000

    Loop challenge

--- main/src/main.rs
+++ main/src/main.rs
@@ -4,19 +4,36 @@ use std::usize;
 /*  
 *
 *
-* Conditions & Loops
+* Cahllenge
 *
 *
 */
 fn main() {
 
-    let mut matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+    let numbers: [i32 ; 14] = [1, 9, -2, 0, 23, 20, -7, 13, 37, 20, 56, -18, 20, 3];
+    let mut max: i32;   
+    let mut min: i32;   
+    let mut mean: f64;
 
-    for row in matrix.iter_mut() {
-        for num in row.iter_mut() {
-            *num += 10;
-            print!("{num}\t");
+    max = numbers[0];
+    min = numbers[0];
+    mean = 0.0;
+
+    for num in numbers {
+        if max < num {
+            max = num;
         }
-        println!();
+        if min > num{
+            min = num;
+        }
+        mean += num as f64; // Add all the numebrs
     }
+
+    mean = mean/numbers.len() as f64; // Calculate mean
+
+    assert_eq!(max, 56);   
+    assert_eq!(min, -18);   
+    assert_eq!(mean, 12.5);   
+    println!("Test Passed!");   
+
 }
