619c75c6c04a7d89d304d1ba601d3c1fd43f8ba5
Simão Lopes <simaolopes98@gmail.com>
2024-11-24 20:29:21 +0000

    Ownership

--- main/src/main.rs
+++ main/src/main.rs
@@ -3,37 +3,19 @@ use std::usize;
 
 /*  
 *
+* Ownership - Varaibles are responsible for freeing their own resources
+* Stack and Heap Memory
 *
-* Cahllenge
-*
+*  
 *
 */
-fn main() {
-
-    let numbers: [i32 ; 14] = [1, 9, -2, 0, 23, 20, -7, 13, 37, 20, 56, -18, 20, 3];
-    let mut max: i32;   
-    let mut min: i32;   
-    let mut mean: f64;
-
-    max = numbers[0];
-    min = numbers[0];
-    mean = 0.0;
-
-    for num in numbers {
-        if max < num {
-            max = num;
-        }
-        if min > num{
-            min = num;
-        }
-        mean += num as f64; // Add all the numebrs
+fn main(){
+    let outer_planet: String;
+    {
+        let inner_planet: String = String::from("Mercury");
+        println!("{inner_planet}");
+        outer_planet = inner_planet; // Passing owernship
     }
-
-    mean = mean/numbers.len() as f64; // Calculate mean
-
-    assert_eq!(max, 56);   
-    assert_eq!(min, -18);   
-    assert_eq!(mean, 12.5);   
-    println!("Test Passed!");   
-
+    
+    println!("{outer_planet}");
 }
