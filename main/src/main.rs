1829794d1de70d16204fbff2c9f881e72405675d
Simão Lopes <simaolopes98@gmail.com>
2024-11-24 16:56:07 +0000

    Loop with return statement

--- main/src/main.rs
+++ main/src/main.rs
@@ -10,10 +10,15 @@ fn main() {
     let x: i32 = if is_odd {1} else {2}; // One-line if
     let mut count: i32= 0;
 
-    loop { // Infinite loop -> CTRL-C to stop
+    let ret: i32= loop { // Infinite loop
+        if count == 10 {
+            break count * 10; // This makes so that the loop turns into an expression
+        }
         count += 1;
         println!("Count {count}");
 
-    }
+    };
+
+    println!("After the loop {ret}");
 
 }
